044fd3667194b4387caa4ed2c737d79185beee07
Alasdair Thomson <alasdair@gzunk.com>
2017-09-16 20:46:29 +0100

    JavaFX Client

---
+++ Client/src/main/java/com/gzunk/jmsperformance/client/AppConfig.java
@@ -0,0 +1,47 @@
+package com.gzunk.jmsperformance.client;
+
+import org.apache.activemq.ActiveMQConnectionFactory;
+import org.apache.activemq.command.ActiveMQQueue;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.annotation.Scope;
+import org.springframework.jms.core.JmsOperations;
+import org.springframework.jms.core.JmsTemplate;
+
+import javax.jms.ConnectionFactory;
+import javax.jms.Destination;
+
+@Configuration
+@PropertySource("classpath:/application.properties")
+public class AppConfig {
+
+    public static final String ADDRESS = "tcp://HOMER:61616";
+
+    @Bean
+    ClientApp clientApp() {
+        return new ClientApp();
+    }
+
+    @Bean
+    ConnectionFactory connectionFactory() {
+        return new ActiveMQConnectionFactory(ADDRESS);
+    }
+
+    @Bean
+    Destination destination(@Value("${jms.queue.name}") String jmsQueueName) {
+        return new ActiveMQQueue(jmsQueueName);
+    }
+
+    @Bean
+    JmsOperations operations() {
+        return new JmsTemplate(connectionFactory());
+    }
+
+    @Bean
+    @Scope("prototype")
+    MainForm mainForm() {
+        return new MainForm();
+    }
+}
