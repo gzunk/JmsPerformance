c77415bb1899da8fcfb7b7c84e4687e79f461067
Alasdair Thomson <alasdair@gzunk.com>
2017-09-17 12:48:13 +0100

    Update logging

--- Server/src/main/java/com/gzunk/jmsperformance/server/JmspMessageListener.java
+++ Server/src/main/java/com/gzunk/jmsperformance/server/JmspMessageListener.java
@@ -1,17 +1,22 @@
 package com.gzunk.jmsperformance.server;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import javax.jms.*;
 
 public class JmspMessageListener implements MessageListener {
+
+    Logger LOG = LoggerFactory.getLogger(JmspMessageListener.class);
 
     @Override
     public void onMessage(Message message) {
         TextMessage textMessage = (TextMessage)message;
         try {
             String mess = textMessage.getText();
-            System.out.printf("Received: %s\n", mess);
+            LOG.info("Received: {}", mess);
         } catch (JMSException e) {
-            System.out.println("Error");
+            LOG.error(e.getMessage());
         }
     }
 }
